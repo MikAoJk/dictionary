3961a9cad8b5b19fc8304eae79c3b44242a2f532
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-15 20:18:01 +0200

    small ui fix

--- src/components/dictionary.tsx
+++ src/components/dictionary.tsx
@@ -1,7 +1,5 @@
 'use client'
 import {useState} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
 
 const Dictionary = () => {
     const [data, setData] = useState<Data>();
@@ -65,14 +63,18 @@ const Dictionary = () => {
             </div>
             {data && (
                 <div
-                    className="text-left border-[3px] border-[solid] border-[#ccc] p-[20px] rounded-[10px] [box-shadow:0_0_10px_rgba(0,_0,_0,_0.2)] bg-[#fff] capitalize">
+                    className="text-left border-[3px] border-[solid] border-[#ccc] p-[20px] rounded-[10px] [box-shadow:0_0_10px_rgba(0,_0,_0,_0.2)] text-white capitalize">
                     <h2>{data.word}</h2>
                     <button
                         onClick={() => {
                             playAudio();
                         }}
                     >
-                        <FontAwesomeIcon icon={faVolumeUp} size="sm"/>
+                        <svg className="w-[50px] h-[50px] fill-[#8e8e8e]" viewBox="0 0 640 512"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"></path>
+                        </svg>
                     </button>
                     <div className="mt-[20px] w-full">
                         <table>
